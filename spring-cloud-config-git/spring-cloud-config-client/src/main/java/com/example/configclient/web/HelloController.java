46cf1ded3503703d4409887c68f6038807fd5a29
hasakei <446109730@qq.com>
2019-05-23 15:49:45 +0800

    自动获取更新的配置信息

--- spring-cloud-config-git/spring-cloud-config-client/src/main/java/com/example/configclient/web/HelloController.java
+++ spring-cloud-config-git/spring-cloud-config-client/src/main/java/com/example/configclient/web/HelloController.java
@@ -1,6 +1,7 @@
 package com.example.configclient.web;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
  * @date 2019/5/23 15:25
  **/
 @RestController
+// 当配置中心信息更改时，自动将新的配置更新
+@RefreshScope
 public class HelloController {
 
     @Value("${neo.hello}")
